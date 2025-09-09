49f37355cbe69dd80849f60944bc4a726701f8e7
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-09-09 16:57:06 -0300

    Añadido título para la vista Person

--- src/app/person/person.ts
+++ src/app/person/person.ts
@@ -1,5 +1,6 @@
 import { AfterContentInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { TitleService } from '../services/title-service';
 
 @Component({
   selector: 'app-person',
@@ -13,16 +14,22 @@ export class PersonComponent implements OnInit, AfterContentInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
+    private titleService: TitleService,
     private cd: ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
     this.cd.detectChanges();
     this.id = parseInt(this.activatedRoute.snapshot.params['id']);
+    this.setTitle();
   }
 
   ngAfterContentInit(): void {
     this.cd.detectChanges();
+  }
+
+  private setTitle(): void {
+    this.titleService.setTitle(`Person with ID ${this.id}`)
   }
 
 }
