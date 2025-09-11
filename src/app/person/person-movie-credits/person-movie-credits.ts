1729752dbbac56401064e0df7b4057ed8c2d48ac
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-09-11 19:49:42 -0300

    Obtención de ID para vistas Person/Overview y Person/Movie-Credits

--- src/app/person/person-movie-credits/person-movie-credits.ts
+++ src/app/person/person-movie-credits/person-movie-credits.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Person } from '../../classes/person';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-person-movie-credits',
@@ -6,6 +9,40 @@ import { Component } from '@angular/core';
   templateUrl: './person-movie-credits.html',
   styleUrl: './person-movie-credits.scss'
 })
-export class PersonMovieCredits {
+export class PersonMovieCredits implements OnInit, OnDestroy {
+
+  public id: number = 0;
+
+  public person: Person = new Person();
+
+  public loadingPerson: boolean = false;
+
+  public activatedRouteParentSubscription: Subscription | undefined;
+
+  public personFound: boolean = false;
+  public personMovieCreditsError: boolean = false;
+  public errorMessage: string = '';
+
+  constructor(
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.setId();
+
+  }
+
+  private setId(): void {
+    this.activatedRouteParentSubscription = this.activatedRoute.parent?.params.subscribe(params => {
+      this.id = parseInt(params['id']);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.activatedRouteParentSubscription) {
+      this.activatedRouteParentSubscription.unsubscribe();
+    }
+
+  }
 
 }
--- src/app/person/person-overview/person-overview.ts
+++ src/app/person/person-overview/person-overview.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Person } from '../../classes/person';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-person-overview',
@@ -6,6 +9,39 @@ import { Component } from '@angular/core';
   templateUrl: './person-overview.html',
   styleUrl: './person-overview.scss'
 })
-export class PersonOverview {
+export class PersonOverview implements OnInit, OnDestroy {
+
+  public id: number = 0;
+
+  public person: Person = new Person();
+
+  public activatedRouteParentSubscription: Subscription | undefined;
+
+  public personFound: boolean = false;
+  public personOverviewError: boolean = false;
+  public errorMessage: string = '';
+
+  constructor(
+    private activatedRoute: ActivatedRoute
+  ) {
+
+  }
+
+
+  ngOnInit(): void {
+    this.setId();
+  }
+
+  private setId(): void {
+    this.activatedRouteParentSubscription = this.activatedRoute.parent?.params.subscribe(params => {
+      this.id = parseInt(params['id']);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.activatedRouteParentSubscription) {
+      this.activatedRouteParentSubscription.unsubscribe();
+    }
+  }
 
 }
