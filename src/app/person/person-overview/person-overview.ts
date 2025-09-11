e3d372debb8f90358a761efc177b86956f043372
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-09-11 20:22:29 -0300

    Biografía de Persona traspasada de vista Person a vista Person/Overview

--- src/app/person/person-overview/person-overview.ts
+++ src/app/person/person-overview/person-overview.ts
@@ -2,6 +2,8 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Person } from '../../classes/person';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { PersonService } from '../../services/person-service';
 
 @Component({
   selector: 'app-person-overview',
@@ -17,12 +19,17 @@ export class PersonOverview implements OnInit, OnDestroy {
 
   public activatedRouteParentSubscription: Subscription | undefined;
 
+  public loadingPerson: boolean = false;
+
+  public personBiography: string = '';
+
   public personFound: boolean = false;
   public personOverviewError: boolean = false;
   public errorMessage: string = '';
 
   constructor(
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private personService: PersonService
   ) {
 
   }
@@ -35,7 +42,38 @@ export class PersonOverview implements OnInit, OnDestroy {
   private setId(): void {
     this.activatedRouteParentSubscription = this.activatedRoute.parent?.params.subscribe(params => {
       this.id = parseInt(params['id']);
+      this.getPerson();
     });
+  }
+
+  private getPerson(): void {
+    this.personOverviewError = false;
+    this.loadingPerson = true;
+    this.personService.getPerson(this.id)
+    .then((person) => {
+      this.person = person;
+      this.personFound = true;
+      this.setPersonBiography();
+    }).catch((error: HttpErrorResponse) => {
+      this.handleError(error);
+    }).finally(() => {
+      this.loadingPerson = false;
+    });
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    this.personOverviewError = true;
+    this.errorMessage = error.message;
+  }
+
+  public reloadOverview(event: boolean): void {
+    this.getPerson();
+  }
+
+  private setPersonBiography(): void {
+    if (this.person.biography.length !== 0) {
+      this.personBiography = this.person.biography;
+    }
   }
 
   ngOnDestroy(): void {
