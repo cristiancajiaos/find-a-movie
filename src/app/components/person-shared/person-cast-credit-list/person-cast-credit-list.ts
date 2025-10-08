f4194a52b49c229b9c968d0ca1674b66cd87db11
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-10-08 12:16:56 -0300

    Importado servicio MovieService para usar título formateado en vistas Person/Cast-Credits y Person/Crew-Credits para ambos modos de visualización

--- src/app/components/person-shared/person-cast-credit-list/person-cast-credit-list.ts
+++ src/app/components/person-shared/person-cast-credit-list/person-cast-credit-list.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ResponsePersonCastCredit } from '../../../classes/person-movie-credits/response-person-cast-credit';
 import { environment } from '../../../../environments/environment.development';
+import { TitleService } from '../../../services/title-service';
+import { MovieService } from '../../../services/movie-service';
 
 @Component({
   selector: 'app-person-cast-credit-list',
@@ -13,16 +15,33 @@ export class PersonCastCreditList implements OnInit {
 
   public posterSizeSmall: string = '';
   public releaseYear: number = 0;
+  public formattedTitle: string = '';
+  public altPosterText: string = '';
+
+  constructor(
+    private movieService: MovieService
+  ) {}
 
   ngOnInit(): void {
     this.setPoster();
     this.setYear();
   }
 
+  public setTitle(): void {
+    this.formattedTitle = this.movieService.getFormattedMovieTitle(
+      this.castRole.title,
+      this.castRole.original_title,
+      this.castRole.release_date
+    );
+  }
+
   public setPoster(): void {
     this.posterSizeSmall = this.castRole.poster_path
       ? `${environment.imgUrl}${environment.posterSizeSmall}${this.castRole.poster_path}`
       : 'img/default-images/movie_poster_notavailable_w500.png';
+    this.altPosterText = this.castRole.poster_path
+      ? `Poster from the movie ${this.formattedTitle}`
+      : `Poster from the movie ${this.formattedTitle} is not available`;
   }
 
   public setYear(): void {
