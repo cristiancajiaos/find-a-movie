39f53b32e3ca09a05cdd90819686d17dc1d38398
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-10-08 12:16:00 -0300

    Eliminado componente sin usar

--- src/app/components/person-shared/person-shared-module.ts
+++ src/app/components/person-shared/person-shared-module.ts
@@ -4,9 +4,8 @@ import { PersonError } from './person-error/person-error';
 import { PersonErrorFullscreen } from './person-error-fullscreen/person-error-fullscreen';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { PersonNotFound } from './person-not-found/person-not-found';
-import { PersonCrewCredit } from './person-crew-credit/person-crew-credit';
 import { RouterModule } from '@angular/router';
-import { NgbModule, NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PersonCastCreditGrid } from './person-cast-credit-grid/person-cast-credit-grid';
 import { PersonCastCreditList } from './person-cast-credit-list/person-cast-credit-list';
 import { PersonCrewCreditGrid } from './person-crew-credit-grid/person-crew-credit-grid';
@@ -17,7 +16,6 @@ import { PersonCrewCreditList } from './person-crew-credit-list/person-crew-cred
     PersonError,
     PersonErrorFullscreen,
     PersonNotFound,
-    PersonCrewCredit,
     PersonCastCreditGrid,
     PersonCastCreditList,
     PersonCrewCreditGrid,
@@ -33,7 +31,6 @@ import { PersonCrewCreditList } from './person-crew-credit-list/person-crew-cred
     PersonError,
     PersonErrorFullscreen,
     PersonNotFound,
-    PersonCrewCredit,
     PersonCastCreditGrid,
     PersonCastCreditList,
     PersonCrewCreditGrid,
