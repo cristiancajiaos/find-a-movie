3337478cae0e96d7cf3fba65821f2632e8a9ccae
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-10-10 17:02:56 -0300

    Correcciones sobre componentes de grilla/lista en vistas Person/Cast y Person/Crew

--- src/app/components/person-shared/person-cast-credit-grid/person-cast-credit-grid.ts
+++ src/app/components/person-shared/person-cast-credit-grid/person-cast-credit-grid.ts
@@ -38,7 +38,7 @@ export class PersonCastCreditGrid implements OnInit {
   public setPoster(): void {
     this.posterSizeSmall = this.castRole.poster_path
       ? `${environment.imgUrl}${environment.posterSizeSmall}${this.castRole.poster_path}`
-      : '/img/default-images/movie_poster_notavailable_w500.png';
+      : 'img/default-images/movie_poster_notavailable_w500.png';
     this.altPosterText = this.castRole.poster_path
       ? `Poster from the movie ${this.formattedTitle}`
       : `Poster from the movie ${this.formattedTitle} is not available`;
--- src/app/components/person-shared/person-cast-credit-list/person-cast-credit-list.ts
+++ src/app/components/person-shared/person-cast-credit-list/person-cast-credit-list.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ResponsePersonCastCredit } from '../../../classes/person-movie-credits/response-person-cast-credit';
 import { environment } from '../../../../environments/environment.development';
-import { TitleService } from '../../../services/title-service';
 import { MovieService } from '../../../services/movie-service';
 
 @Component({
@@ -38,7 +37,7 @@ export class PersonCastCreditList implements OnInit {
   public setPoster(): void {
     this.posterSizeSmall = this.castRole.poster_path
       ? `${environment.imgUrl}${environment.posterSizeSmall}${this.castRole.poster_path}`
-      : '/img/default-images/movie_poster_notavailable_w500.png';
+      : 'img/default-images/movie_poster_notavailable_w500.png';
     this.altPosterText = this.castRole.poster_path
       ? `Poster from the movie ${this.formattedTitle}`
       : `Poster from the movie ${this.formattedTitle} is not available`;
