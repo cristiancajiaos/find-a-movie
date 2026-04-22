b2f44f02cd271a4862a1f7c8d406f02e6dc56072
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-04-22 13:06:17 -0400

    Implementado maqueteo de dropdown de búsqueda en buscador de películas

--- src/app/components/layout/navbar/search-movie/search-movie.ts
+++ src/app/components/layout/navbar/search-movie/search-movie.ts
@@ -2,7 +2,7 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { faMagnifyingGlass, IconDefinition } from '@fortawesome/free-solid-svg-icons';
-
+import { NgbDropdown } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-search-movie',
   standalone: false,
@@ -15,10 +15,20 @@ export class SearchMovie implements OnInit {
 
   public searchMovieForm: FormGroup = new FormGroup({});
 
+  public movies: any = [
+    {id: 2501, title: 'The Bourne Identity', year: 2002, img: 'https://placehold.co/25'},
+    {id: 2502, title: 'The Bourne Supremacy', year: 2004, img: 'https://placehold.co/25'},
+    {id: 2503, title: 'The Bourne Ultimatum', year: 2007, img: 'https://placehold.co/25'},
+    {id: 49040, title: 'The Bourne Legacy', year: 2012, img: 'https://placehold.co/25'},
+    {id: 324668, title: 'Jason Bourne', year: 2016, img: 'https://placehold.co/25' }
+  ];
+
   public placeholder: string = 'Eg. Star Wars';
   public ariaLabel: string = 'Search Movie';
 
   @ViewChild('searchMovieInput') searchMovieInput: ElementRef;
+
+  @ViewChild('myDrop') myDrop: NgbDropdown;
 
   constructor(
     private fb: FormBuilder,
@@ -38,6 +48,19 @@ export class SearchMovie implements OnInit {
       return;
     }
     this.router.navigate(['search', 'movie', searchQuery]);
+  }
+
+  public toggleMovieSearchDropdown(): void {
+    const inputMovie: string = this.searchMovieForm.controls['movieSearch'].value;
+    if (inputMovie.length >= 3) {
+      this.myDrop.open();
+    } else {
+      this.myDrop.close();
+    }
+  }
+
+  public goToMovieClose(movie: any): void {
+    this.myDrop.close();
   }
 
 }
