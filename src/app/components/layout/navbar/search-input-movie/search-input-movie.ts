061a0781316bb6870fa47ab2fd10ca76247c2b32
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-04-22 18:08:27 -0400

    Correcciones de la búsqueda predictiva al escribir en el input de búsqueda de películas

--- src/app/components/layout/navbar/search-input-movie/search-input-movie.ts
+++ src/app/components/layout/navbar/search-input-movie/search-input-movie.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import { faMagnifyingGlass, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { faList, faMagnifyingGlass, faSpinner, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { NgbDropdown } from '@ng-bootstrap/ng-bootstrap';
 import { ResponseSearchMovie } from '../../../../classes/response-search-movie';
 import { ResponseMovieResult } from '../../../../classes/response-search-movie/response-movie-result';
@@ -17,6 +17,8 @@ import { HttpErrorResponse } from '@angular/common/http';
 export class SearchInputMovie implements OnInit {
 
   public searchIcon: IconDefinition = faMagnifyingGlass;
+  public loadingIcon: IconDefinition = faSpinner;
+  public resultsIcon: IconDefinition = faList;
 
   public searchMovieForm: FormGroup = new FormGroup({});
 
@@ -30,7 +32,6 @@ export class SearchInputMovie implements OnInit {
   public ariaLabel: string = 'Search Movie';
 
   @ViewChild('searchMovieInput') searchMovieInput: ElementRef;
-
   @ViewChild('myDrop') myDrop: NgbDropdown;
 
   constructor(
@@ -54,14 +55,36 @@ export class SearchInputMovie implements OnInit {
     this.router.navigate(['search', 'movie', searchQuery]);
   }
 
-  public toggleMovieSearchDropdown(): void {
+  public toggleMovieSearchDropdownBySearch(): void {
+    this.movieResults = [];
+  }
+
+  public toggleMovieSearchDropdownByInput(): void {
     const inputMovie: string = this.searchMovieForm.controls['movieSearch'].value;
     if (inputMovie.length >= 3) {
       this.searchMovie();
-      this.myDrop.open();
+      this.openMovieSearchDropdown();
     } else {
-      this.myDrop.close();
+      this.closeMovieSearchDropdown();
     }
+  }
+
+  public toggleMovieSearchDropdownByClick(): void {
+    if (this.myDrop.isOpen && this.movieResults.length > 0) {
+      this.myDrop.toggle();
+    }
+  }
+
+  public openMovieSearchDropdown(): void {
+    this.myDrop.open();
+  }
+
+  public closeMovieSearchDropdown(): void {
+    this.myDrop.close();
+  }
+
+  public toggleMovieSearchDropdown(): void {
+    this.myDrop.toggle();
   }
 
   public searchMovie(): void {
@@ -76,11 +99,6 @@ export class SearchInputMovie implements OnInit {
     }).finally(() => {
       this.loadingSearchMovie = false;
     });
-  }
-
-  public handleMovieResults(responseSearchMovie: ResponseSearchMovie): void {
-    this.movieResults = responseSearchMovie.results;
-    console.log(this.movieResults);
   }
 
   public goToMovieClose(value: boolean): void {
