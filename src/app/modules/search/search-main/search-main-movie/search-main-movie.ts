7d42c41ac60d2725e5599c1df73f43805e88d2a2
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-05-08 12:33:03 -0400

    Limpieza de componentes SearchMainMovie y SearchMainPerson

--- src/app/modules/search/search-main/search-main-movie/search-main-movie.ts
+++ src/app/modules/search/search-main/search-main-movie/search-main-movie.ts
@@ -1,7 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
-import { faFilm, faSearch, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit } from '@angular/core';
+import { faFilm, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-search-main-movie',
@@ -11,32 +9,10 @@ import { faFilm, faSearch, IconDefinition } from '@fortawesome/free-solid-svg-ic
 })
 export class SearchMainMovie implements OnInit {
 
-  private fb = inject(FormBuilder);
-  private router = inject(Router);
-
   public bgImage: string = 'img/bg/search-main-bg-2.jpg';
 
-  public searchIcon: IconDefinition = faSearch;
   public movieIcon: IconDefinition = faFilm;
 
-  public placeholder: string = 'Eg. Star Wars';
-
-  public movieSearchForm: FormGroup = new FormGroup({});
-
   ngOnInit(): void {
-    this.movieSearchForm = this.fb.group({
-      movieSearch: new FormControl(''),
-    });
   }
-
-  public sendMovieQuery(): void {
-    const movieQuery = this.movieSearchForm.value['movieSearch'];
-
-    if (!movieQuery || movieQuery.length == 0) {
-      return;
-    }
-
-    this.router.navigate(['search', 'movie', movieQuery]);
-  }
-
 }
--- src/app/modules/search/search-main/search-main-person/search-main-person.ts
+++ src/app/modules/search/search-main/search-main-person/search-main-person.ts
@@ -1,7 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
-import { faSearch, faUser, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit } from '@angular/core';
+import { faUser, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-search-main-person',
@@ -11,32 +9,10 @@ import { faSearch, faUser, IconDefinition } from '@fortawesome/free-solid-svg-ic
 })
 export class SearchMainPerson implements OnInit {
 
-  private fb = inject(FormBuilder);
-  private router = inject(Router);
-
   public bgImage: string = 'img/bg/search-main-bg-1.jpg';
 
-  public searchIcon: IconDefinition = faSearch;
   public userIcon: IconDefinition = faUser;
 
-  public placeholder: string = 'Eg. Steven Spielberg';
-
-  public personSearchForm: FormGroup = new FormGroup({});
-
   ngOnInit(): void {
-    this.personSearchForm = this.fb.group({
-      personSearch: new FormControl(''),
-    });
   }
-
-  public sendPersonQuery(): void {
-    const personQuery = this.personSearchForm.value['personSearch'];
-
-    if (!personQuery || personQuery.length == 0) {
-      return;
-    }
-
-    this.router.navigate(['search', 'person', personQuery]);
-  }
-
 }
