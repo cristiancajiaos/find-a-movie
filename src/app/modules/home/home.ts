1542fa45bf043fc20a6086f5cf3a21afbd53a7d9
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-05-09 11:20:55 -0400

    Limpieza de vista principal

--- src/app/modules/home/home.ts
+++ src/app/modules/home/home.ts
@@ -12,14 +12,6 @@ export class Home implements OnInit {
 
   private titleService = inject(TitleService);
 
-  public filmIcon: IconDefinition = faFilm;
-
-  public welcomeToText: string = 'Welcome to';
-  public findAMovieTitle: string = 'Find a Movie';
-  public paragraphText: string = 'Search the movies you love, and the people who work in them, all in one place';
-
-  public bgImage: string = 'img/bg/home-bg.jpg';
-
   ngOnInit(): void {
     this.titleService.setDefaultTitle();
   }
