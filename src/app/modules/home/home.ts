4554c1fcbdfe250480f6de600362e6bcbfb2e984
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-07-01 15:14:26 -0400

    Limpieza de componente Home

--- src/app/modules/home/home.ts
+++ src/app/modules/home/home.ts
@@ -1,6 +1,5 @@
 import { Component, inject, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { TitleService } from '../../services/title-service';
-import { faFilm, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
