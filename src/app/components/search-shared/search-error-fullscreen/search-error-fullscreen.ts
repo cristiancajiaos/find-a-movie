d1d59ce4cbef23e6d326735062eb1b10481052fa
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-07-14 13:00:36 -0400

    SearchError: Eliminado OnI nit sin usar

--- src/app/components/search-shared/search-error-fullscreen/search-error-fullscreen.ts
+++ src/app/components/search-shared/search-error-fullscreen/search-error-fullscreen.ts
@@ -8,16 +8,12 @@ import { faRepeat, IconDefinition } from '@fortawesome/free-solid-svg-icons';
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './search-error-fullscreen.scss'
 })
-export class SearchErrorFullscreen implements OnInit {
+export class SearchErrorFullscreen {
 
   public repeatIcon: IconDefinition = faRepeat;
 
   @Input() errorMessage: string = '';
   @Output() reload: EventEmitter<boolean> = new EventEmitter<boolean>();
-
-  ngOnInit(): void {
-
-  }
 
   public pressReload(): void {
     this.reload.emit(true);
