5d721d678fd2c6215182b5474e79a28dc5d97cb9
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-07-14 13:12:46 -0400

    Eliminados componentes HomeMovie y HomePerson

--- src/app/modules/home/home-module.ts
+++ src/app/modules/home/home-module.ts
@@ -4,8 +4,6 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing-module';
 import { Home } from './home';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { HomeMovie } from './home-movie/home-movie';
-import { HomePerson } from './home-person/home-person';
 import { LayoutModule } from '../../components/layout/layout-module';
 import { HomeTrending } from './home-trending/home-trending';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -14,8 +12,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     Home,
-    HomeMovie,
-    HomePerson,
     HomeTrending
   ],
   imports: [
