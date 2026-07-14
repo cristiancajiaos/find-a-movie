2bec4bf366188ca379caf6bd9a3b9b642efb642d
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-07-14 12:53:54 -0400

    PersonErrorFullscreen: Eliminado OnInit sin usar, eliminado fondo sin usar

--- src/app/components/person-shared/person-error-fullscreen/person-error-fullscreen.ts
+++ src/app/components/person-shared/person-error-fullscreen/person-error-fullscreen.ts
@@ -8,20 +8,14 @@ import { faRepeat, IconDefinition } from '@fortawesome/free-solid-svg-icons';
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './person-error-fullscreen.scss'
 })
-export class PersonErrorFullscreen implements OnInit {
+export class PersonErrorFullscreen {
 
-  public bgImage: string = 'img/bg/bg_generic_2.jpg';
   public repeat: IconDefinition = faRepeat;
 
   @Input() errorMessage: string = '';
   @Output() reload: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  ngOnInit(): void {
-
-  }
-
   public pressReload(): void {
     this.reload.emit(true);
   }
-
 }
